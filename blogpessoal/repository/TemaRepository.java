32a167cd9a4ca9a61f76e4685ec6170285109868
Jessica Cavalcante <je.cavalcante@outlook.com>
2023-02-06 20:16:11 -0300

    adicionando security no BlogPessoal

---
+++ blogpessoal/repository/TemaRepository.java
@@ -0,0 +1,17 @@
+package org.generation.blogpessoal.repository;
+
+
+	import java.util.List;
+
+	import org.generation.blogpessoal.model.Tema;
+	import org.springframework.data.jpa.repository.JpaRepository;
+	import org.springframework.data.repository.query.Param;
+	import org.springframework.stereotype.Repository;
+
+	@Repository
+	public interface TemaRepository extends JpaRepository<Tema, Long> {
+
+	 public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+
+	}
+
